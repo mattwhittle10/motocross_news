00b3b01b9a34f55b49ff192cc25b414403caf0d2
Whittle <matt.whittle10@gmail.com>
2019-09-21 23:37:32 -0600

    css added for notes box section

--- public/app.js
+++ public/app.js
@@ -16,8 +16,8 @@ $(document).on("click", "p", function() {
     .then(function(data) {
       console.log(data);
       $("#notes").append("<h2>" + data.title + "</h2>");
-      $("#notes").append("<input id='titleinput' name='title' >");
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<div>Title:<br><input id='titleinput' name='title'></div>");
+      $("#notes").append("<div>Notes:<textarea id='bodyinput' name='body'></textarea></div>");
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
       if (data.note) {
