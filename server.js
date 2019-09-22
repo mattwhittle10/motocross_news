d785b6d01a853160f1342e8e749af8cdd9d853f0
Whittle <matt.whittle10@gmail.com>
2019-09-22 00:04:16 -0600

    added the new connection string for heroku

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/motocrossdb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/motocrossdb", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 app.get("/scrape", function(req, res) {
   axios.get("https://motocrossactionmag.com/").then(function(response) {
